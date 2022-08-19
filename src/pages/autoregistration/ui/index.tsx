891ca84b4fbd04d8f1d7ef7081dea2276ec00c2a
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-08-19 17:40:10 +0500

    refactor: refactored ui of pages/autoregistration

--- src/pages/autoregistration/ui/index.tsx
+++ src/pages/autoregistration/ui/index.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
+import styles from './styles.module.scss';
 
 const AutoRegistration: FC = () => {
   console.log('delete this console.log');
   return (
-    <div>
-      1
+    <div className={styles.page}>
+      <h2>AutoRegistration</h2>
+      <input type="text" className={styles.input} />
+      <input type="text" className={styles.input} />
     </div>
   );
 };
