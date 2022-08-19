b309d6c6fd75204a27ee3a518bc472be886544ee
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-08-20 01:10:25 +0500

    added!: added baseurl in tsconfig.json, also added shared/api/users

---
+++ src/shared/api/users/index.ts
@@ -0,0 +1,13 @@
+type UsersKeys = {
+login: string,
+password: string,
+};
+
+const USERS: UsersKeys[] = [
+    {login: 'medvedev', password: '1'},
+    {login: 'darker', password: '2'},
+    {login: 'markerov', password: '3'},
+    {login: 'chernov', password: '4'},
+];
+
+export {USERS};
