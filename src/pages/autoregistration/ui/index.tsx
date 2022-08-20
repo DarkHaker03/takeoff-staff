105df53cc09e4a4cc8a76f4e8076a20ae2109063
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-08-20 09:24:57 +0500

    added!: added useInput, also refactor ui of pages/autoregistration

--- src/pages/autoregistration/ui/index.tsx
+++ src/pages/autoregistration/ui/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
+import useInput from 'shared/hooks/useInput';
 import styles from './styles.module.scss';
 
 const AutoRegistration: FC = () => {
-  console.log('delete this console.log');
+  const [name, setName] = useInput();
+
   return (
     <div className={styles.page}>
       <h2>AutoRegistration</h2>
-      <input type="text" className={styles.input} />
+      <input type="text" value={name} onChange={setName} className={styles.input} />
       <input type="text" className={styles.input} />
     </div>
   );
---
+++ src/shared/hooks/useInput.ts
@@ -0,0 +1,17 @@
+import React, { useCallback, useState } from 'react';
+
+const useInput = () => {
+  const [state, setState] = useState<string>();
+  const onChange = useCallback((
+    e: React.ChangeEvent<HTMLInputElement>| React.ChangeEvent<HTMLTextAreaElement>|string,
+  ) => {
+    const value: string = typeof e !== 'string'
+      ? e.target.value
+      : e;
+    setState(value);
+  }, []);
+
+  return [state, onChange] as const;
+};
+
+export default useInput;
