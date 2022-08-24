835f8cc44fa86485cf726c82b2fe38ab7204455c
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-08-24 20:27:48 +0500

    refactor: optimize list of contacts in ui pages/contacts/ui/contactsList

--- src/pages/contacts/ui/contactsList/index.tsx
+++ src/pages/contacts/ui/contactsList/index.tsx
@@ -1,23 +1,33 @@
-import { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { ContactsModel } from 'pages/contacts';
+import { ContactKeys } from 'shared/api/contacts';
 import { observer } from 'mobx-react-lite';
 import styles from './styles.module.scss';
 
+type Props = {
+  handleClick: any,
+  contact: ContactKeys,
+}
+
+const Item = React.memo(({ handleClick, contact: { id, name, number } }: Props) => (
+  <div className={styles.item} onClick={() => handleClick(id)}>
+    <div className={styles.name}>{name}</div>
+    <div className={styles.number}>{number}</div>
+  </div>
+));
+
 const ContactsList: FC = observer(() => {
-  const handleClick = (id: number) => {
+  const handleClick = useCallback((id: number) => {
     const { changeMode, toggle } = ContactsModel.Popup;
     const { setSelectemItemId } = ContactsModel.Contacts;
     changeMode('redactAndDelete');
     toggle();
     setSelectemItemId(id);
-  };
+  }, []);
   return (
     <div>
       {ContactsModel.Contacts.filter.map((contact) => (
-        <div className={styles.item} onClick={() => handleClick(contact.id)} key={contact.id}>
-          <div className={styles.name}>{contact.name}</div>
-          <div className={styles.number}>{contact.number}</div>
-        </div>
+        <Item handleClick={handleClick} contact={contact} key={contact.id} />
       ))}
     </div>
   );
